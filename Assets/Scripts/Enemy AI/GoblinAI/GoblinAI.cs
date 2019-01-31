14fa9b136314639bf49c022abcf53d6d4d9f4906
Daniel <43864783+Samurike@users.noreply.github.com>
2019-01-31 11:01:09 -0600

    v 1.0
    Version 1.0

--- Assets/Scripts/Enemy AI/GoblinAI/GoblinAI.cs
+++ Assets/Scripts/Enemy AI/GoblinAI/GoblinAI.cs
@@ -8,39 +8,45 @@ public class GoblinAI : MonoBehaviour {
 
     private Animator anim;
     private Rigidbody rb;
-    private float speed = 5f;
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] float chaseRange = 5f;
+
+    [SerializeField] float attackRadius = 5f;
+    [SerializeField] float moveRadius = 5f;
 
     [SerializeField] Transform baseLocation;
     [SerializeField] Transform waypoint1;
     [SerializeField] Transform waypoint2;
+
+    [SerializeField] ChaScript player;
 
     // Use this for initialization
     void Start() {
 
         anim = this.GetComponent<Animator>();
         rb = this.GetComponent<Rigidbody>();
-        
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<ChaScript>();
+
     }
 
     // Update is called once per frame
     void Update() {
 
-        StartCoroutine(Wander());
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
     }
 
 
     private IEnumerator Wander()
     {
+        yield return new WaitForSeconds(2f);
 
-        yield return new WaitForSeconds(4f);
-
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
-        yield return new WaitForSeconds(4f);
-
-        transform.Translate(Vector3.back * speed * Time.deltaTime);
-
-    } 
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
 
 }
