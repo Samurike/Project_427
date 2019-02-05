4582280c57787600e5ca98efaae9ce1e3a781877
Daniel <dal3742@louisiana.edu>
2019-02-05 16:51:33 -0600

    Lots of Stuff (see description)
    Healthbar and  Mana bar added
    (not dynamic)
    Quest UI added (not dynamic)
    Organized Assets

--- Assets/Scripts/EntityScripts/Health.cs
+++ Assets/Scripts/EntityScripts/Health.cs
@@ -1,24 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
 
     public float maxHealth;
     public float currentHealth;
 
-	// Use this for initialization
-	void Start () {
+    public float maxEnergy;
+    public float currentEnergy;
+
+    [SerializeField] Image imgHP;
+    [SerializeField] Image imgEN;
+
+    // Use this for initialization
+    void Start () {
         currentHealth = maxHealth;
-	}
+        currentEnergy = maxEnergy;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	}
+
+        if (transform.tag == "Player")
+        {
+            imgHP.fillAmount = currentHealth / maxHealth;
+            imgEN.fillAmount = currentEnergy / maxEnergy;
+        }
+
+    }
 
     public void takeDamage(float num)
     {
+
         currentHealth -= num;
+
         Debug.Log(currentHealth);
         if(currentHealth <= 0)
         {
---
+++ Assets/Scripts/Misc/TurnOffScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffScript : MonoBehaviour {
+
+    bool act = false;
+
+	// Use this for initialization
+	void Start () {
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (act)
+            {
+                act = false;
+            }
+            else
+            {
+                act = true;
+            }
+
+            transform.Find("Menu").gameObject.SetActive(act);
+        }
+
+	}
+}
