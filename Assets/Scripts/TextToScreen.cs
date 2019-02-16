d6effb95db06220e1eafaa23e8c5ebd2842bc198
Daniel <43864783+Samurike@users.noreply.github.com>
2019-02-15 19:39:32 -0600

    Big Push read desc
    Fireball working, walls in adoran, scene managament through scenes working, slight tutorial setup, forest area in adoran, health orb bug found

---
+++ Assets/Scripts/TextToScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextToScreen : MonoBehaviour {
+
+    [SerializeField] Text txt;
+    [SerializeField] Text txtOutline;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            txt.enabled = true;
+            txtOutline.enabled = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            txt.enabled = false;
+            txtOutline.enabled = false;
+        }
+    }
+}
