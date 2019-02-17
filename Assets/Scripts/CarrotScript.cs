27fa33c56639bbf828664d126301394457ae33c6
Daniel <43864783+Samurike@users.noreply.github.com>
2019-02-16 20:28:55 -0600

    Rabbit Boss Fight
    anything related to rabbit boss fight, a few map transitions and some text

---
+++ Assets/Scripts/CarrotScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotScript : MonoBehaviour {
+
+    [SerializeField] float carrotDmg;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<Health>().takeDamage(carrotDmg); 
+        }
+
+        if (transform.gameObject.tag != "BarrierCarrot")
+        {
+            Debug.Log("Hi");
+            Destroy(gameObject, 0.3f);
+        }
+
+
+
+    }
+}
