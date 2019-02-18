6cfe21b4b1dd29bdfde631743f33152b70c8f066
Daniel <43864783+Samurike@users.noreply.github.com>
2019-02-17 18:45:30 -0600

    Final Daniel Commit

---
+++ Assets/Scripts/LoadDeathScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadDeathScene : MonoBehaviour {
+
+    [SerializeField] string scene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(GetComponent<Health>().currentHealth <= 0)
+        {
+            SceneManager.LoadScene(scene);
+            
+        }
+
+	}
+}
